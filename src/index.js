30cef777472a7056548cfa3d007575101fd0780f
Darko Tasevski <c.dtasevski@gogoair.com>
2018-10-17 16:26:09 +0200

    feat: Redux setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
+
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store/store';
 
+const store = configureStore();
 const rootEl = document.getElementById('root');
 const Root = () => (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 );
 
 const render = () => {
