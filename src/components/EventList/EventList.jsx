e4c3a39e7800229eebd7ae0cfcf25d6803806503
Darko Tasevski <c.dtasevski@gogoair.com>
2018-10-24 19:37:58 +0200

    feat: Infinite scroll

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// Be careful when working with this lib, improper use can cause really nasty CPU spikes
+import InfiniteScroll from 'react-infinite-scroller';
 
 import EventListItem from './EventListItem';
 
-const EventList = ({ events = [], deleteEvent }) => (
+const EventList = ({ events = [], deleteEvent, getMoreEvents, isLoading, moreEvents }) => (
 	<div>
-		{events.map(e => (
-			<EventListItem event={e} deleteEvent={deleteEvent} key={e.id} />
-		))}
+		{events.length && (
+			<InfiniteScroll
+				pageStart={0}
+				loadMore={getMoreEvents}
+				hasMore={!isLoading && moreEvents}
+				initialLoad={false}
+			>
+				{events.map(e => (
+					<EventListItem event={e} deleteEvent={deleteEvent} key={e.id} />
+				))}
+			</InfiniteScroll>
+		)}
 	</div>
 );
 
